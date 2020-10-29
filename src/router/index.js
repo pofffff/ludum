6024d4c4f60b564b71ad68aa716fbf8113c3cf0f
Matilda Höglund <hoglund.matilda@gmail.com>
2020-10-29 10:59:22 +0100

    router links

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Profile from '../views/Profile.vue';
+import GameInfo from '../views/GameInfo.vue';
+import Store from '../views/Store.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +12,21 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile,
+  },
+  {
+    path: '/game-info',
+    name: 'Game Info',
+    component: GameInfo,
+  },
+  {
+    path: '/store',
+    name: 'Store',
+    component: Store,
   },
 ];
 
