bf17f00f97b2e5cc3b70c7cd440dae84cb09b331
Matilda Höglund <hoglund.matilda@gmail.com>
2020-10-29 12:04:37 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <GhostButton :button_text="button_text" />
-    <PrimaryButton :button_text="button_text" />
-    <SecondaryButton :button_text="button_text" />
   </div>
 </template>
 
 <script>
-import GhostButton from "@/components/global/buttons/GhostButton";
-import PrimaryButton from "@/components/global/buttons/PrimaryButton";
-import SecondaryButton from "@/components/global/buttons/SecondaryButton";
-
 export default {
-  components: { GhostButton, PrimaryButton, SecondaryButton },
   data: () => {
-    return {
-      button_text: "Primary",
-    };
+    return {};
   },
 };
 </script>
