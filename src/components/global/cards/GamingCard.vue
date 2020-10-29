3304f1c026d793864db53ca6cb6ecc4a45187b96
Stefan Isgren <stefan.isgren@gmail.com>
2020-10-29 13:13:03 +0100

    added card components
    .

---
+++ src/components/global/cards/GamingCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <article class="gaming_card"> 
+    <h1>GAMING CARD</h1>
+  </article>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Store.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <h1>TEST</h1>
+   
+  </section>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+   
+  }
+}
+</script>
+
+<style>
+
+</style>
