5575e703be05652a744af8d23473528dd5931911
jensengbg-fredrik-berntsson <fredrikberntsson@hotmail.com>
2020-11-02 11:59:54 +0100

    fred game section

---
+++ src/views/GameInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="game__info wrapper">
+  <GameTrailer />
+  <PcRequirement />
+  <GameStreams />
+  </section>
+</template>
+
+<script>
+import GameTrailer from "@/components/GameTrailer"
+import PcRequirement from "@/components/PcRequirement"
+import GameStreams from "@/components/GameStreams"
+export default {
+components: { GameTrailer, PcRequirement, GameStreams }
+}
+</script>
+
+<style>
+
+</style>
