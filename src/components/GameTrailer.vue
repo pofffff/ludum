7de9d914c96548121170ab32d79bdf9f7459bd2f
jensengbg-fredrik-berntsson <fredrikberntsson@hotmail.com>
2020-11-03 15:34:51 +0100

    started with pre-views

--- src/components/GameTrailer.vue
+++ src/components/GameTrailer.vue
@@ -1,6 +1,15 @@
 <template>
 <section clasS="game__trailer">
   <h3>Valorant</h3>
+<iframe width="750" height="400" src="https://www.youtube.com/embed/e_E9W2vsRbQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<section class="examples">
+<iframe src="https://www.youtube.com/embed/e_E9W2vsRbQ" class="small__examples" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<img src="@/assets/valorant/valorant_1.jpg" alt="" class="small__examples" >
+<img src="@/assets/valorant/valorant_2.jpg" alt="" class="small__examples" >
+<img src="@/assets/valorant/valorant_3.jpg" alt="" class="small__examples" >
+<img src="@/assets/valorant/valorant_4.jpg" alt="" class="small__examples" >
+<img src="@/assets/valorant/valorant_5.jpg" alt="" class="small__examples" >
+</section>
 </section>
 </template>
 
@@ -10,6 +19,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.examples {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  width: 750px;
 
+  .small__examples{
+    width: 100px;
+    height: 70px;
+  }
+}
 </style>
