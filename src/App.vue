7d909ef3f9b83e73426401584979db37eea4ab23
Matilda Höglund <48841764+jensengbg-matilda-hoglund@users.noreply.github.com>
2020-11-03 09:55:39 +0100

    friends

--- src/App.vue
+++ src/App.vue
@@ -35,4 +35,7 @@ export default {
   padding: 0;
   margin: 0;
 }
+
+
+
 </style>
--- src/components/FriendsList.vue
+++ src/components/FriendsList.vue
@@ -7,24 +7,24 @@
     <section class="container__friends_list">
       <li
         class="friend__card"
-        v-for="(friend, index) in this.friendsOnline"
-        :key="index"
+        v-for="friend in this.friendsOnline"
+        :key="friend.name"
       >
         <i class="fas fa-user"></i>
         <section class="friend__details online">
           <h5>{{ friend.name }}</h5>
-          <p>{{ friend.playing }}</p>
+          <p class="p1 online">{{ friend.playing }}</p>
         </section>
       </li>
       <li
         class="friend__card"
-        v-for="(friend, index) in this.friendsOffline"
-        :key="index"
+        v-for="friend in this.friendsOffline"
+        :key="friend.name"
       >
         <i class="fas fa-user"></i>
         <section class="friend__details offline">
           <h5>{{ friend.name }}</h5>
-          <p>{{ friend.playing }}</p>
+          <p class="p1 offline">{{ friend.playing }}</p>
         </section>
       </li>
     </section>
@@ -37,15 +37,15 @@ export default {
   data: () => {
     return {
       friendsOnline: [
-        { name: "Executioner", playing: "Playing: World Of Warcraft" },
+        { name: "Executioner", playing: "World Of Warcraft" },
         {
           name: "Cartman",
-          playing: "Playing: Counter Strike - Global Offensive",
+          playing: "Counter Strike",
         },
-        { name: "Stisen", playing: "Playing: Heartstone" },
-        { name: "DonaldChump", playing: "Playing: World Of Warcraft" },
-        { name: "Bumbibjörnen", playing: "Playing: The Long Dark" },
-        { name: "Hejhopp", playing: "Playing: Fifa 2020" },
+        { name: "Stisen", playing: "Heartstone" },
+        { name: "DonaldChump", playing: "World Of Warcraft" },
+        { name: "Bumbibjörnen", playing: "The Long Dark" },
+        { name: "Hejhopp", playing: "Fifa 2020" },
       ],
       friendsOffline: [
         { name: "ThisIsMyAlias", playing: "last seen 6 hours ago" },
@@ -59,19 +59,59 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+
 .profile__friends_list {
   grid-column: 3/4;
   padding: $L 0;
+  
 }
 
 .header__friends_list {
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-bottom: $M;
 }
 
 .container__friends_list {
   overflow: scroll;
   height: 550px;
+  padding-right: $S;
+
+  .friend__card {
+  box-shadow: $small_card_box_shadow;
+  border-radius: 3px;
+  height: 75px;
+  margin: $XS 0;
+  display: flex;
+  align-items: center;
+  padding: $S;
+
+  .online {
+    color: $site_color;
+  }
+
+  .offline {
+    color: $secondary_text_color;
+  }
+
+  i {
+    font-size: $L;
+    margin-right: $M;
+  }
+
+  h5 {
+    margin-bottom: $XS;
+  }
+  
 }
+
+.friend__card:hover {
+  background: $dark;
+}
+}
+
+
+
+
 </style>
