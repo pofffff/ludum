7787cc7f88cf705e21d8176a0eb4a345d1096e45
jensengbg-fredrik-berntsson <fredrikberntsson@hotmail.com>
2020-11-05 14:03:39 +0100

    game-info working

--- src/components/GameDescription.vue
+++ src/components/GameDescription.vue
@@ -1,48 +1,50 @@
 <template>
   <section class="gameinfo__description">
-  <div>
-  <img src="@/assets/valorant/valorant_3.jpg" alt="valorant" class="valo">
-  <h3>Description</h3>
-  <p class="p2">It's a 5v5 tac-shooter matchup to plant or defuse the Spike in a one-life-per-round, first to 13 series. More than guns and bullets, you'll choose an Agent armed with adaptive, swift, and lethal abilities that create opportunities to let your gunplay shine.Creativity is your greatest weapon.</p>
-  </div>
+    <div>
+      <img src="@/assets/valorant/valorant_3.jpg" alt="valorant" class="valo" />
+      <h3>Description</h3>
+      <p class="p2">
+        It's a 5v5 tac-shooter matchup to plant or defuse the Spike in a
+        one-life-per-round, first to 13 series. More than guns and bullets,
+        you'll choose an Agent armed with adaptive, swift, and lethal abilities
+        that create opportunities to let your gunplay shine.Creativity is your
+        greatest weapon.
+      </p>
+    </div>
   </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
 .gameinfo__description {
-width: 350px;
-height: 350px;
-background-color: $card;
-border-radius: $border-radius;
-
+  width: 350px;
+  height: 350px;
+  background-color: $card;
+  border-radius: $border-radius;
+  box-shadow: $small_card_box_shadow;
 
   img {
     width: 350px;
   }
-  h3{
+  h3 {
     text-align: center;
     margin: $S;
   }
-  p{
+  p {
     padding: $S;
   }
 }
 @media only screen and (max-width: 700px) {
-.gameinfo__description {
-  width: 85vw;
-  margin-bottom: $M;
-
-  img{
+  .gameinfo__description {
     width: 85vw;
+    margin-bottom: $M;
+
+    img {
+      width: 85vw;
+    }
   }
 }
-}
-
 </style>
--- src/components/GameTrailer.vue
+++ src/components/GameTrailer.vue
@@ -1,29 +1,58 @@
 <template>
-<section clasS="game__trailer">
-<iframe src="https://www.youtube.com/embed/e_E9W2vsRbQ" class="video" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<section class="examples">
-<iframe src="https://www.youtube.com/embed/e_E9W2vsRbQ" class="small__examples" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-<img src="@/assets/valorant/valorant_1.jpg" alt="" class="small__examples" >
-<img src="@/assets/valorant/valorant_2.jpg" alt="" class="small__examples" >
-<img src="@/assets/valorant/valorant_3.jpg" alt="" class="small__examples" >
-<img src="@/assets/valorant/valorant_4.jpg" alt="" class="small__examples" >
-<img src="@/assets/valorant/valorant_5.jpg" alt="" class="small__examples" >
-</section>
-</section>
+  <section clasS="game__trailer">
+    <iframe
+      src="https://www.youtube.com/embed/e_E9W2vsRbQ"
+      class="video"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+    <section class="examples">
+      <iframe
+        src="https://www.youtube.com/embed/e_E9W2vsRbQ"
+        class="small__examples"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <img
+        src="@/assets/valorant/valorant_1.jpg"
+        alt=""
+        class="small__examples"
+      />
+      <img
+        src="@/assets/valorant/valorant_2.jpg"
+        alt=""
+        class="small__examples"
+      />
+      <img
+        src="@/assets/valorant/valorant_3.jpg"
+        alt=""
+        class="small__examples"
+      />
+      <img
+        src="@/assets/valorant/valorant_4.jpg"
+        alt=""
+        class="small__examples"
+      />
+      <img
+        src="@/assets/valorant/valorant_5.jpg"
+        alt=""
+        class="small__examples"
+      />
+    </section>
+  </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-
- .video {
-    height: 400px;
-    width: 750px;
-  }
+.video {
+  height: 400px;
+  width: 750px;
+}
 
 .examples {
   display: flex;
@@ -32,20 +61,24 @@ export default {
   width: 750px;
   margin-bottom: $XL;
 
-  .small__examples{
+  .small__examples {
     width: 100px;
     height: 70px;
     margin-top: $M;
   }
 }
 @media only screen and (max-width: 700px) {
-.video {
-  width: 85vw;
-}
-.examples {
-  width: 85vw;
-  overflow-x: scroll;
-  overflow-y: hidden;
-}
+  .video {
+    width: 85vw;
+  }
+  .small__examples {
+    margin: 0 0.2em;
+  }
+  .examples {
+    width: 85vw;
+    overflow-x: scroll;
+    overflow-y: hidden;
+      justify-content: space-between;
+  }
 }
 </style>
